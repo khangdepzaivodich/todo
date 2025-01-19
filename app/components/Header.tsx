75897e10fe824e8d106a78425f338554730b397e
khang <phantuankhanghcmue@gmail.com>
2025-01-19 20:03:49 +0700

    todo app

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="text-white p-5 bg-cyan-500 font-semibold text-center text-2xl">
+      Todo List
+    </div>
+  );
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,103 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { FaRegCircle, FaRegCircleCheck } from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  interface Todo {
+    task: string;
+    isDone: boolean;
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [currentTodo, setCurrentTodo] = useState<Todo>({
+    task: "",
+    isDone: false,
+  });
+
+  const handleClick = () => {
+    if (currentTodo?.task.trim()) {
+      setTodos([...todos, currentTodo]);
+      setCurrentTodo({ task: "", isDone: false });
+    }
+  };
+
+  const handleCheck = (id: number) => {
+    const updatedTodo = todos.map((todo, index) => {
+      if (index === id) {
+        return { ...todo, isDone: !todo.isDone };
+      }
+      return todo;
+    });
+    setTodos(updatedTodo);
+  };
+
+  const handleDelete = (id: number) => {
+    const updatedTodo = todos.filter((todo, index) => index !== id);
+    setTodos(updatedTodo);
+  };
+
+  return (
+    <>
+      <div className="ml-10 md:ml-40 mr-10 md:mr-40 pt-10">
+        <div className="flex mb-5">
+          <input
+            className="bg-blue-100 py-3 px-4 rounded-l-3xl text-xl w-full focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300"
+            placeholder="Add a new task..."
+            onChange={(e) => {
+              setCurrentTodo({ task: e.target.value, isDone: false });
+            }}
+            value={currentTodo?.task}
+          />
+          <button
+            className="bg-blue-500 text-white font-semibold text-lg rounded-r-3xl px-5 py-3 transition-transform transform hover:scale-105"
+            onClick={handleClick}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Add
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <div
+          className={`mt-5 ${
+            todos.length ? "bg-blue-50" : ""
+          } p-5 rounded-2xl shadow-lg`}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {todos.length === 0 ? (
+            <p className="text-center text-gray-500">No tasks to show</p>
+          ) : (
+            todos.map((todo, index) => (
+              <div
+                key={index}
+                className="bg-white p-4 rounded-lg shadow-md mb-3 flex items-center justify-between hover:bg-gray-100 transition-all duration-300"
+              >
+                <div
+                  className="flex items-center gap-5 cursor-pointer"
+                  onClick={() => handleCheck(index)}
+                >
+                  {todo.isDone ? (
+                    <FaRegCircleCheck color="green" size={24} />
+                  ) : (
+                    <FaRegCircle color="gray" size={24} />
+                  )}
+                  <div
+                    className={`text-xl transition-all duration-300 ${
+                      todo.isDone ? "line-through text-gray-500" : ""
+                    }`}
+                  >
+                    {todo.task}
+                  </div>
+                </div>
+
+                <button
+                  className="text-red-500 hover:text-red-700 transition-all duration-300"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </>
   );
 }
